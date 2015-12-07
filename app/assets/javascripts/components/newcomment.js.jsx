6d8459a825ab971920a3844cf90bde90ea094570
EOligney <Eoligney@arthrex.com>
2015-12-07 15:08:43 -0500

    resolved CSFR exception

--- app/assets/javascripts/components/newcomment.js.jsx
+++ app/assets/javascripts/components/newcomment.js.jsx
@@ -1,15 +1,16 @@
 var NewComment = React.createClass({
   propTypes: {
     blog_id: React.PropTypes.string,
-    url: React.PropTypes.string,
-    csrf_token: React.PropTypes.string
+    path: React.PropTypes.string,
+    token: React.PropTypes.string
   },
 
   render: function() {
     return (
-      <form action={this.props.blog_id + "/comments"} method="post">
+      <form role="comment" accept-charset="UTF-8" action={this.props.path} method="post">
         <label for="body">Body:</label>
         <input type="text" name="comment[body]"></input><br></br>
+        <input type="hidden" name="authenticity_token" value={this.props.token}></input>
         <input type="submit" value="Submit"></input>
       </form>
     );
